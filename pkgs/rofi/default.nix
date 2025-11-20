a58c8b749b37af618c54d26d33c3436e19a2b407
DevNLKoen <94992822+DevNLKoen@users.noreply.github.com>
2025-11-20 23:30:19 +0100

    fixed formatting

--- pkgs/rofi/default.nix
+++ pkgs/rofi/default.nix
@@ -3,15 +3,15 @@
   wrappers,
   ...
 }:
-  (wrappers.wrapperModules.rofi.apply {
-    pkgs = pkgs;
-    plugins = [pkgs.rofi-games];
-    settings = {
-      modes = "window,drun,games";
-      show-icons = true;
-      sidebar-mode = true;
-      drun-display-format = "{name}";
-      terminal = "kitty";
-    };
-    theme = builtins.toString (./theme.rasi);
-  }).wrapper
+(wrappers.wrapperModules.rofi.apply {
+  pkgs = pkgs;
+  plugins = [pkgs.rofi-games];
+  settings = {
+    modes = "window,drun,games";
+    show-icons = true;
+    sidebar-mode = true;
+    drun-display-format = "{name}";
+    terminal = "kitty";
+  };
+  theme = builtins.toString (./theme.rasi);
+}).wrapper
