fcfd075ab9cc10eba79a7cdbf522b69067c38eda
DevNLKoen <94992822+DevNLKoen@users.noreply.github.com>
2026-03-11 11:21:26 +0100

    added distrobox

--- hosts/umbryn/configuration.nix
+++ hosts/umbryn/configuration.nix
@@ -13,6 +13,11 @@
     ./../configuration.nix
   ];
 
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+
   # nyxen options from modules
   nyxen = {
     name = "umbryn";
@@ -35,6 +40,9 @@
 
   programs = {
     firefox.enable = true;
+    sway.enable = true;
+    hyprland.enable = true;
+    wayfire.enable = true;
     kdeconnect.enable = true;
     pinnacle = {
       enable = true;
@@ -50,6 +58,8 @@
     tuigreet
     pulseaudio
     playerctl
+
+    distrobox
   ];
 
   services = {
